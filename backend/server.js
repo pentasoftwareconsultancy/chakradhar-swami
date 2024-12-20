b356902f3f4bceca643aee60425a5cc2749c0ada
ankita-satdeve <ankitasatdeve03@gmail.com>
2024-12-20 16:36:10 +0530

    deleted

--- backend/server.js
+++
@@ -1,5 +0,0 @@
-
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/chakradhar', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch((error) => console.error('Error connecting to MongoDB:', error));
